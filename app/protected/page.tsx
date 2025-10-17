81096cf033e6e7a28053c74ef95457ff333911e7
Mathias-Erni-Morel <erni.massagen@gmail.com>
2025-10-17 16:56:08 +0200

    Update page.tsx

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,27 +1,6 @@
-import { auth, signOut } from 'app/auth';
+// app/protected/page.tsx
+import { redirect } from "next/navigation";
 
-export default async function ProtectedPage() {
-  let session = await auth();
-
-  return (
-    <div className="flex h-screen bg-black">
-      <div className="w-screen h-screen flex flex-col space-y-5 justify-center items-center text-white">
-        You are logged in as {session?.user?.email}
-        <SignOut />
-      </div>
-    </div>
-  );
-}
-
-function SignOut() {
-  return (
-    <form
-      action={async () => {
-        'use server';
-        await signOut();
-      }}
-    >
-      <button type="submit">Sign out</button>
-    </form>
-  );
+export default function ProtectedAlias() {
+  redirect("/dashboard");
 }
